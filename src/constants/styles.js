8b5d9634908fdf1c22f1fd30a5cf36a95714426a
Eliza Nitoi <eliza.nitoi@gmail.com>
2019-12-14 15:04:35 +0200

    Added some style constants (#22)

---
+++ src/constants/styles.js
@@ -0,0 +1,44 @@
+
+export const fontWeight = {
+  light: "300",
+  regular: "400",
+  normal: "500",
+  bold: "700",
+  extrabold: "900"
+};
+
+export const spacing = {
+  small: 10,
+  normal: 15,
+  big: 20,
+  huge: 40,
+  xHuge: 60
+};
+
+export const iconSize = {
+  small: 20,
+  normal: 25,
+  big: 30,
+  large: 40,
+  xlarge: 50
+};
+
+export const fontSize = {
+  stiny: 10,
+  tiny: 12,
+  default: 14,
+  medium: 16,
+  large: 24,
+  xlarge: 32,
+  xxlarge: 46
+};
+
+export const colors = {
+  white: "#fff",
+  black: "#000",
+  green: "#42B28A",
+  purple: "#5E69BC",
+  red: "#FF0000",
+  blue: "#3A89C2",
+  darkGrey: "#232840",
+};
